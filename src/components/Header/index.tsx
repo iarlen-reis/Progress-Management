b74fe8af065a9d9d6a19d2e5221946b2feb0c552
Iarlen <iarlensr@hotmail.com>
2024-02-23 04:24:33 -0300

    refactor: modifying components Header, Input and Textarea.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,28 @@
-'use client'
-import React from 'react'
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuList,
-} from '@/components/ui/navigation-menu'
 import Link from 'next/link'
+import React from 'react'
+import LogoutButton from '../LogoutButton'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 
-const Header = () => {
+const Header = async () => {
+  const session = await getServerSession(authOptions)
+
   return (
-    <header className="bg-slate-900 w-full py-4 text-white">
-      <div className="container px-3 md:px-6">
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <Link href="/" className="font-fredoka font-medium text-2xl">
-                Home
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+    <header className="bg-slate-900 w-full py-5 text-white">
+      <div className="flex items-center justify-between container px-3 md:px-6">
+        <Link href="/" className="text-xl font-medium">
+          Progress
+          <span className="text-slate-500  font-fredoka font-bold">
+            Management
+          </span>
+        </Link>
+        <ul>
+          {session?.user && (
+            <li>
+              <LogoutButton />
+            </li>
+          )}
+        </ul>
       </div>
     </header>
   )
