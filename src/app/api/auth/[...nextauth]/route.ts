9be3810676ae5ecbde4ed385eb1ab6a9e5d0f9d3
Iarlen <iarlensr@hotmail.com>
2024-02-23 04:22:18 -0300

    feat: adding user model relationship to task model and configuring prisma adapter.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,23 @@
 import { env } from '@/lib/env'
 import NextAuth, { AuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/github'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from '@/lib/prisma'
 
 export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: env.GITHUB_ID,
       clientSecret: env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id
+      return Promise.resolve(session)
+    },
+  },
   secret: env.NEXTAUTH_SECRET,
   pages: {
     signIn: '/login',
